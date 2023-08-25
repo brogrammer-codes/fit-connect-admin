7ca2e8383a2c801020986eb888df83e811976696
ralph-p <mr.pereira91@gmail.com>
2023-08-25 14:53:04 -0400

    updated nav bar style, added card component and dashboard body component

--- admin-connect/app/(pages)/dashboard/(routes)/page.tsx
+++ admin-connect/app/(pages)/dashboard/(routes)/page.tsx
@@ -1,10 +1,27 @@
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
 import { Heading } from "@/components/ui/heading";
+
+const DashboardBody: React.FC<{}> = () => {
+  return (
+    <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
+      <Card>
+        <CardHeader>
+          <CardTitle>Clients</CardTitle>
+        </CardHeader>
+      </Card>
+    </div>
+  );
+};
 
 export default function Dashboard() {
   return (
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
-        <Heading title="Dashboard" description="A dashboard of your clients, programs, and activities."/>
+        <Heading
+          title="Dashboard"
+          description="A dashboard of your clients, programs, and activities."
+        />
+        <DashboardBody />
       </div>
     </div>
   );
--- admin-connect/components/navbar.tsx
+++ admin-connect/components/navbar.tsx
@@ -1,6 +1,7 @@
 import { SignInButton, UserButton, auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 import { MainNav } from "@/components/main-nav";
+import Link from "next/link";
 
 const Navbar = async () => {
   const { userId } = auth();
@@ -11,8 +12,8 @@ const Navbar = async () => {
 
   return (
     <div className="border-b">
-      <div className="flex h-16 items-center px-4">
-        <span>Fit Connect</span>
+      <div className="flex h-16 items-center px-4 bg-slate-700">
+        <Link href={'/'}><span className="text-2xl font-bold text-lime-500">Fit</span> <span className="text-xl font-bold text-slate-400">Connect</span></Link>
 
         <div className="ml-auto flex items-center space-x-4">
           {/* <ThemeToggle /> */}
