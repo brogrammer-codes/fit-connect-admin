8c284dfc97f056d00e71da8d4972d99d2f20175f
ralph-p <mr.pereira91@gmail.com>
2023-08-25 15:32:03 -0400

    added client display

--- admin-connect/app/(pages)/dashboard/(routes)/page.tsx
+++ admin-connect/app/(pages)/dashboard/(routes)/page.tsx
@@ -1,19 +1,49 @@
-import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+"use client";
+import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
 import { Heading } from "@/components/ui/heading";
+import { Activity, Client, Plan } from "@prisma/client";
+import axios from "axios";
+import { User2 } from "lucide-react";
+import { useEffect, useState } from "react";
 
-const DashboardBody: React.FC<{}> = () => {
+interface DashboardInterface {
+  clientList: Client[] | [];
+  // planList: Plan[] | []
+  // activityList: Activity[] | []
+}
+
+const DashboardBody: React.FC<DashboardInterface> = ({
+  clientList,
+  // planList,
+  // activityList,
+}) => {
   return (
     <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
       <Card>
-        <CardHeader>
-          <CardTitle>Clients</CardTitle>
+        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+          <CardTitle className="text-2xl font-bold">
+            {clientList.length === 5
+              ? `Top 5 Clients`
+              : `Clients (${clientList.length})`}
+          </CardTitle>
         </CardHeader>
+        <CardContent>
+          {clientList.length ? <ul>{clientList.map((client) => <div className="flex space-x-3" key={client.id}><User2/> <span>{client.name}</span> </div>)}</ul> : "No Clients, add some!"}
+        </CardContent>
       </Card>
     </div>
   );
 };
 
 export default function Dashboard() {
+  const [clientList, setClientList] = useState<Client[] | []>([]);
+  useEffect(() => {
+    const getDashboard = async () => await axios.get(`/api/dashboard`);
+    getDashboard().then(({ data }) => {
+      setClientList(data.clientList);
+    });
+  }, []);
+
   return (
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
@@ -21,7 +51,7 @@ export default function Dashboard() {
           title="Dashboard"
           description="A dashboard of your clients, programs, and activities."
         />
-        <DashboardBody />
+        <DashboardBody clientList={clientList} />
       </div>
     </div>
   );
