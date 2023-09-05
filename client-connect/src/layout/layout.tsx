523c8849657f2b612c34838966794f78be05d180
ralph-p <mr.pereira91@gmail.com>
2023-09-05 14:19:44 -0400

    fixed indents, added client name to head

--- client-connect/src/layout/layout.tsx
+++ client-connect/src/layout/layout.tsx
@@ -1,12 +1,9 @@
-import Head from "next/head";
 import type { PropsWithChildren } from "react";
 
 export default function Layout(props: PropsWithChildren) {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-slate-400">
-      <div className="flex flex-col space-y-3 ">
-        {props.children}
-      </div>
+    <main className="flex min-h-screen flex-col items-center justify-between bg-slate-400 p-24">
+      <div className="flex flex-col space-y-3 ">{props.children}</div>
     </main>
   );
 }
--- client-connect/src/pages/client/[clientId].tsx
+++ client-connect/src/pages/client/[clientId].tsx
@@ -1,27 +1,30 @@
 import type { NextPage, GetStaticProps } from "next";
 import { generateSSGHelper } from "~/server/helpers/ssgHelper";
 import { api } from "~/utils/api";
+import Head from "next/head";
 
 const ClientPage: NextPage<{ clientId: string }> = ({ clientId }) => {
-  const {
-    data,
-  } = api.client.getClient.useQuery({ clientId });
-  if(!data) return <div>No Client with that ID</div>
-  const {client} = data
+  const { data } = api.client.getClient.useQuery({ clientId });
+  if (!data) return <div>No Client with that ID</div>;
+  const { client } = data;
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 ">
-      <div className="flex flex-col space-y-3">
-        <span className="text-3xl font-semibold">Welcome {client.name}</span>
-        <span className="text-xl">Achieve Your Fitness Goals, Your Way</span>
-        <span className="">
-          {`Plans (${client.planList.length})`}
-        </span>
-        <span className="">
-          To use Fit Connect you need a Coach to create an admin account or you
-          can visit the Admin portal to sign-up and add yourself as a client.
-        </span>
-      </div>
-    </main>
+    <>
+      <Head>
+        <title>{client.name}</title>
+      </Head>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24 ">
+        <div className="flex flex-col space-y-3">
+          <span className="text-3xl font-semibold">Welcome {client.name}</span>
+          <span className="text-xl">Achieve Your Fitness Goals, Your Way</span>
+          <span className="">{`Plans (${client.planList.length})`}</span>
+          <span className="">
+            To use Fit Connect you need a Coach to create an admin account or
+            you can visit the Admin portal to sign-up and add yourself as a
+            client.
+          </span>
+        </div>
+      </main>
+    </>
   );
 };
 export const getStaticProps: GetStaticProps = (context) => {
