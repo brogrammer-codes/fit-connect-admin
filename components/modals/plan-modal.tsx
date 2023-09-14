f8ed44a989fe2d518d384c44fa8f721c33985eea
ralph-p <mr.pereira91@gmail.com>
2023-09-13 20:47:18 -0400

    fixed copy on question

--- components/modals/plan-modal.tsx
+++ components/modals/plan-modal.tsx
@@ -54,7 +54,7 @@ export const PlanModal: React.FC<PlanModalProps> = ({
     >
       <div className="pt-6 space-y-2 flex flex-col items-center justify-end w-full">
         <PlanPicker items={planList} currentPlan={plan} onPlanSelect={onPlanSelect} />
-        {plan ? <div> Do you want to assign this to {plan.name}?</div> : null}
+        {plan ? <div> Do you want to <span className=" font-sans">{plan.name}</span> to {clientName} ?</div> : null}
       </div>
       <div className="pt-6 space-x-2 flex items-center justify-end w-full">
         {plan ? (<Button disabled={loading} variant="outline" onClick={() => { onClose(); onConfirm(plan.id) }}>
